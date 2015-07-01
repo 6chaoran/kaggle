aac8df196c22b298029975ef50b23a3450028b02
6chaoran <6chaoran@Chaorans-iMac.local>
2015-07-01 19:26:50 +0800

    add commets

--- titanic/titanic.R
+++ titanic/titanic.R
@@ -1,19 +1,28 @@
-setwd("~/Dropbox/kaggle/titanic")
+#create/set the working directory
+if (!file.exists('~/Desktop/kaggle_titanic')) dir.create('~/Desktop/kaggle_titanic',recursive = T)
+setwd("~/Desktop/kaggle_titanic")
+
+#download the data set from github
+if (!file.exists('train.csv')) download.file('https://raw.githubusercontent.com/6chaoran/kaggle/master/titanic/train.csv'
+,'train.csv',method='curl')
+if (!file.exists('test.csv')) download.file('https://raw.githubusercontent.com/6chaoran/kaggle/master/titanic/test.csv'
+,'test.csv',method='curl')
+
 #reading train/test data
 train<-read.csv('train.csv',na.strings=c('NA',''), stringsAsFactors=F)
 test<-read.csv('test.csv',na.strings=c('NA',''),stringsAsFactors=F)
 
-#checking the missing data
-require(Amelia)
-missmap(train,col=c('yellow','black'),main='Titanic Train Data',legend=F)
 #combine train/test data for pre-processing
 train$Cat<-'train'
 test$Cat<-'test'
 test$Survived<-NA
 full<-rbind(train,test)
 
-#Embarked
-full$Embarked[is.na(full$Embarked)]<-'S'
+#checking the missing data
+require(Amelia)
+missmap(train,col=c('yellow','black'),main='Titanic Train Data',legend=F)
+
+#Create Features for model fitting
 #Extract Title from Name
 full$Title<-sapply(full$Name,function(x) strsplit(x,'[.,]')[[1]][2])
 full$Title<-gsub(' ','',full$Title)
@@ -21,27 +30,19 @@ aggregate(Age~Title,full,median)
 full$Title[full$Title %in% c('Capt', 'Don', 'Major','Jonkheer')] <- 'Sir'
 full$Title[full$Title %in% c('Dona','the Countess' )] <- 'Lady'
 full$Title[full$Title %in% c('Mlle','Mme','Ms')] <- 'Miss'
+
 #Adding FamilySize
 full$FamilySize<-full$Parch+full$SibSp+1
-
-#Fare
-#simply replace the one missing Fare data with median, due to skewed distribution of Fare
-full$Fare[is.na(full$Fare)]<-median(full$Fare,na.rm=T)
-
-#Age
-#decision tree (regression) method to predict missing Age data
-library(rpart)
-fit.Age<-rpart(Age[!is.na(Age)]~Pclass+Title+Sex+SibSp+Parch+Fare,data=full[!is.na(full$Age),],method='anova')
-full$Age[is.na(full$Age)]<-predict(fit.Age,full[is.na(full$Age),])
 
 #Adding Mother
 full$Mother<-0
 full$Mother[full$Sex=='female' & full$Parch>0 & full$Age>18 & full$Title!='Miss']<-1
+
 #Adding Child
 full$Child<-0
 full$Child[full$Parch>0 & full$Age<=12]<-1
 
-#FamilyId
+#Adding FamilyId
 Surname<-sapply(full$Name,function(x) strsplit(x,'[.,]')[[1]][1])
 FamilyId<-paste0(full$FamilySize,Surname)
 full$FamilyId<-factor(FamilyId)
@@ -64,9 +65,23 @@ Pos<-kmeans(num,3)
 full$CabinPos<-'UNK'
 full$CabinPos[!is.na(full$num)]<-Pos$cluster
 full$CabinPos<-factor(full$CabinPos)
-
 full$num<-NULL
 
+#Dealing with missing data (Emarked/Fare/Age)
+#Embarked
+full$Embarked[is.na(full$Embarked)]<-'S'
+
+#Fare
+#simply replace the one missing Fare data with median, due to skewed distribution of Fare
+full$Fare[is.na(full$Fare)]<-median(full$Fare,na.rm=T)
+
+#Age
+#regression tree method to predict missing Age data
+library(rpart)
+fit.Age<-rpart(Age[!is.na(Age)]~Pclass+Title+Sex+SibSp+Parch+Fare,data=full[!is.na(full$Age),],method='anova')
+full$Age[is.na(full$Age)]<-predict(fit.Age,full[is.na(full$Age),])
+
+#factorize the categorical data
 full<-transform(full,
                 Pclass=factor(Pclass),
                 Sex=factor(Sex),
@@ -78,45 +93,53 @@ full<-transform(full,
                 Deck=factor(Deck)
 )
 
+#split into train/test dataset
 train<-full[full$Cat=='train',]
 test<-full[full$Cat=='test',]
 train$Survived<-factor(train$Survived)
 
-##Train using Caret
+#Train using Caret package
+#setting 10 folds cross validation for a fair model evaluation
 library(caret)
+set.seed(99)
 control=trainControl(method='repeatedcv',number=10,repeats=10)
+#optimized setting for neural network, prefine for fast re-run
 grid.nnet<-expand.grid(size=1,decay=0.1)
-#grid.rf<-expand.rf(mtry=96)
+
+#train with decision tree model
 fit.rp<-train(Survived~FamilySize+CabinPos+Deck+Pclass+Sex+Age+SibSp+Parch+Fare+Embarked+Title+Mother+Child,
               data=train,method='rpart',trControl=control)
 
-fit.bl<-train(Survived~FamilySize+CabinPos+Deck+Pclass+Sex+Age+SibSp+Parch+Fare+Embarked+Title+Mother+Child,
-              data=train,method='bayesglm',trControl=control,family=binomial(link='logit'))
-
+#train with logistic regression
 fit.glm<-train(Survived~FamilySize+CabinPos+Deck+Pclass+Sex+Age+SibSp+Parch+Fare+Embarked+Title+Mother+Child,
                data=train,method='glm',trControl=control,family=binomial(link='logit'))
 
+#train with bayesian logistic regression
+fit.bl<-train(Survived~FamilySize+CabinPos+Deck+Pclass+Sex+Age+SibSp+Parch+Fare+Embarked+Title+Mother+Child,
+              data=train,method='bayesglm',trControl=control,family=binomial(link='logit'))
+
+#train with random forest
 fit.rf<-train(Survived~FamilySize+CabinPos+Deck+Pclass+Sex+Age+SibSp+Parch+Fare+Embarked+Title+Mother+Child,
               data=train,method='rf',trControl=control)
 
+#train with neural network
 fit.nnet<-train(Survived~FamilySize+CabinPos+Deck+Pclass+Sex+Age+SibSp+Parch+Fare+Embarked+Title+Mother+Child,
                 data=train,method='nnet',trControl=control,maxit=1000,trace=F,tuneGrid=grid.nnet)
 
+#tabulating the prediction accuracy
+acc.glm<-paste0(round(max(fit.glm$results$Accuracy),4)*100,'%')
+acc.bl<-paste0(round(max(fit.bl$results$Accuracy),4)*100,'%')
+acc.rp<-paste0(round(max(fit.rp$results$Accuracy),4)*100,"%")
+acc.rf<-paste0(round(max(fit.rf$results$Accuracy),4)*100,"%")
+acc.nnet<-paste0(round(max(fit.nnet$results$Accuracy),4)*100,'%')
 
-paste0(round(max(fit.glm$results$Accuracy),4)*100,'%')
-paste0(round(max(fit.bl$results$Accuracy),4)*100,'%')
-paste0(round(max(fit.rp$results$Accuracy),4)*100,"%")
-paste0(round(max(fit.rf$results$Accuracy),4)*100,"%")
-paste0(round(max(fit.nnet$results$Accuracy),4)*100,'%')
+cat('prediction accucary with logisitc regreesion is ',acc.glm,'.\n')
+cat('prediction accucary with bayesian logisitc regreesion is ',acc.bl,'.\n')
+cat('prediction accucary with decision tree model is ',acc.rp,'.\n')
+cat('prediction accucary with random forest  is ',acc.rf,'.\n')
+cat('prediction accucary with nerual network is ',acc.nnet,'.\n')
 
-
-library(party)
-fit<-cforest(Survived~FamilyId2+CabinPos+Deck+Pclass+Sex+Age+SibSp+Parch+Fare+Embarked+Title+Mother+Child+Deck,data=train,controls=cforest_unbiased(ntree=1500, mtry=3))
-plot(fit.rf,main='randomForest error rate')
-imp<-importance(fit.rf,type='1')
-imp<-imp[order(imp),]
-barplot(imp,las=1,cex.names=0.7,col='blue',horiz=T)
-
+#writing submission with nerual network method
 test$Survived<-predict(fit.nnet,test)
 submission<-test[,1:2]
 write.csv(submission,'submission_nnet.csv',row.names=F)
